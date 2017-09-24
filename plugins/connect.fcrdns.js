f8321a38a8efcdaefadd2e2555032655930a6b66
tclaybor <tsclay@znbx.me>
2017-09-24 18:07:27 -0500

    Initial commit...
    //TODO
    - Implement DKIM
    - Setup dev team aliases
    - Get a CA signed TLS cert
    - Documentation for certain plugins is currently lacking
    - Integrate DB queries

---
+++ plugins/connect.fcrdns.js
@@ -0,0 +1,6 @@
+'use strict';
+
+exports.register = function () {
+    this.logerror('deprecated! Moved to https://github.com/haraka/haraka-plugin-fcrdns');
+};
+
---
+++ plugins/rcpt_to.qmail_deliverable.js
@@ -0,0 +1,5 @@
+exports.register = function () {
+    const plugin = this;
+    plugin.logerror("ERROR: this plugin has been replaced by https://github.com/haraka/haraka-plugin-qmail-deliverable");
+};
+
